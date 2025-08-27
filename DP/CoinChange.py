4e7d852b625a419127095530197741730125b627
NobodyDtr76 <truongbf18@gmail.com>
2025-08-27 20:15:40 +0700

    Dynamic Programming

---
+++ DP/CoinChange.py
@@ -0,0 +1,10 @@
+if __name__=="__main__":
+    Coins=[2,4,6,9]
+    Total=38
+    Money=[Total]*(Total+1)
+    Money[0]=0
+    for money in range(Total+1):
+        for coin in Coins:
+            if money>=coin and Money[money-coin]+1<Money[money]:
+                Money[money]=Money[money-coin]+1
+    print(Money[Total])
