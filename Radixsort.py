d962b3835317a1767fa0c1bc3e80f7877f25abf4
NobodyDtr76 <truongbf18@gmail.com>
2025-08-27 14:31:59 +0700

    Radixsort

--- Radixsort.py
+++ Radixsort.py
@@ -2,7 +2,7 @@
 def bubbleSort(arr):
     n=len(arr)
     for i in range(n):
-        for j in range(i+1 , n-i-1):
+        for j in range(i+1 , n):
                if arr[i] > arr[j]:
                     arr[i], arr[j]=arr[j], arr[i]
 
