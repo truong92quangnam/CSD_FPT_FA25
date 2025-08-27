b49a0d81717aac35cc7865b883d5d3a43c619c99
NobodyDtr76 <truongbf18@gmail.com>
2025-08-27 13:14:15 +0700

    Quick Sort

---
+++ QuickSort.py
@@ -0,0 +1,25 @@
+#Đây cũng là một dạng thuật toán được dùng trong thuật toán chia để trị để giải quyết bài toán
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+def quicksort(arr, low=0, high=None):
+    if high is None:
+        high = len(arr) - 1  
+
+    if low < high:
+        pivot_index = partition(arr, low, high)
+        quicksort(arr, low, pivot_index - 1) 
+        quicksort(arr, pivot_index + 1, high) 
+
+if __name__ == "__main__":
+    my_array = [64, 34, 25, 12, 22, 11, 90, 5]
+    quicksort(my_array)
+    print("Sorted array:", my_array)
